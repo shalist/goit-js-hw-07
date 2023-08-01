33c4726078624c1cc9d3208869189fc36a6fc3c4
shalist <NE401960@gmail.com>
2023-08-01 22:41:57 +0300

    hw-7_task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const container = document.querySelector('.gallery');
+
+function createMarcup(arr) {
+    return arr.map(({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </li>`
+    ).join('');
+};
+
+container.insertAdjacentHTML('beforeend', createMarcup(galleryItems));
+
+container.addEventListener('click', findOriginalImage);
+
+function findOriginalImage(evt) {
+    evt.preventDefault();
+    const instance = basicLightbox.create(`<img src=${evt.target.dataset.source}>`);
+    instance.show();
+};
