c08f328991dbf5aae6823c0b297a4e7ec26195f0
shalist <NE401960@gmail.com>
2023-08-01 23:33:32 +0300

    hw-7_Task-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -10,10 +10,10 @@ function createMarcup(arr) {
         `<li class="gallery__item">
         <a class="gallery__link" href="${original}">
             <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
             />
         </a>
     </li>`
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const container = document.querySelector('.gallery');
+
+function createMarcup(arr) {
+    return arr.map(({ preview, original, description }) =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>
+        </li>`
+    ).join('');
+};
+
+container.insertAdjacentHTML('beforeend', createMarcup(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+});
